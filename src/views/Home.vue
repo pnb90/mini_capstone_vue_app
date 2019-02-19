cf1047d80d64f16d8874f0d6342d1b4310438b06
pnb90 <phuocbui90@gmail.com>
2019-02-19 10:35:51 -0600

    added show, edit, update

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,108 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>New Product</h1>
+    <div>
+      <div>
+        Name: <input v-model="newProductName">
+      </div>
+      <div>
+        Description: <input v-model="newProductDescription">
+      </div>
+      <div>
+        Price: <input v-model="newProductPrice">
+      </div>
+      <div>
+        Image URL: <input v-model="newProductImageUrl">
+      </div>
+      <button v-on:click="createProduct()">Create</button>
+    </div>
+    <h1> All Products </h1>
+    <div v-for="product in products">
+      <h2> {{ product.name }} </h2>
+        <img v-bind:src="product.image_url" v-bind:alt="product.name">
+      <div>
+        <button v-on:click="currentProduct = product"> More Info </button>
+      </div>
+      <div v-if="product === currentProduct">
+        <p>Description: {{product.description}} </p>
+        <p>Price: {{product.formatted.price}} </p>
+        <div>
+          <h4> Edit Product </h4>
+          <div>
+            <div>
+              Name: <input v-model="product.name">
+            </div>
+            <div>
+              Description: <input v-model="product.description">
+            </div>
+            <div>
+              Price: <input v-model="product.price">
+            </div>
+            <div>
+              Image URL: <input v-model="product.image_url">
+            </div>
+            <button v-on:click="updateProduct(product)"> Update </button>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
-<script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
-export default {
-  name: 'home',
-  components: {
-    HelloWorld
+<style>
+  img{
+    width: 300px;
+    height: 250px;
   }
-}
+</style>
+
+<script>
+  var axios = require("axios");
+
+  export default {
+    data: function() {
+      return {
+        products: [],
+        newProductName: "",
+        newProductDescription: "",
+        newProductPrice: "",
+        newProductImageUrl: "",
+        currentProduct: {}
+      };
+    },
+    created: function() {
+      axios.get("/api/products")
+        .then(response => {
+          this.products = response.data;
+        });
+    },
+    methods: {
+      createProduct: function() {
+        var params = {
+                      name: this.newProductName,
+                      description: this.newProductDescription,
+                      price: this.newProductPrice,
+                      image_url: this.newProductImageUrl
+                      };
+        axios.post("/api/products", params)
+          .then(response => {
+            console.log("Product Successfully Created!", response.data);
+            this.products.push(response.data)
+          });
+      },
+      updateProduct: function(inputProduct) {
+        var params = {
+                      name: inputProduct.name,
+                      description: inputProduct.description,
+                      price: inputProduct.price,
+                      image_url: inputProduct.image_url
+                    };
+        axios.patch("/api/products/" + inputProduct.id, params)
+        .then(response => {
+          console.log("Successfully updated product!", response.data);
+          inputProduct = response.data
+        });
+      }
+    }
+    };
 </script>
